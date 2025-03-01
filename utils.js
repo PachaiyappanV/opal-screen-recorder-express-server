8ffaa2d7da9186077b7864125eb08d915eb07c52
PachaiyappanV <pachaiyappan394@gmail.com>
2025-03-01 22:04:26 +0530

    feat: enhance audio extraction utility to return promise with output file path

--- utils.js
+++ utils.js
@@ -1,19 +1,24 @@
 const ffmpeg = require("fluent-ffmpeg");
-const extractAudioAndCompress = (fileName) => {
-  const inputVideo = `temp_video/${fileName}`;
-  const finalAudio = `temp_audio/${fileName}.mp3`;
 
-  ffmpeg(inputVideo)
-    .output(finalAudio)
-    .noVideo()
-    .audioBitrate("64k") // Compress audio
-    .on("end", () => {
-      console.log("Audio extracted and compressed successfully!");
-    })
-    .on("error", (err) => {
-      console.error("Error:", err);
-    })
-    .run();
+const extractAudioAndCompress = (fileName) => {
+  return new Promise((resolve, reject) => {
+    const inputVideo = `temp_video/${fileName}`;
+    const finalAudio = `temp_audio/${fileName}.mp3`;
+
+    ffmpeg(inputVideo)
+      .output(finalAudio)
+      .noVideo()
+      .audioBitrate("64k") // Compress audio
+      .on("end", () => {
+        console.log("Audio extracted and compressed successfully!");
+        resolve(finalAudio); // Resolve with output file path
+      })
+      .on("error", (err) => {
+        console.error("Error:", err);
+        reject(err);
+      })
+      .run();
+  });
 };
 
 module.exports = {
