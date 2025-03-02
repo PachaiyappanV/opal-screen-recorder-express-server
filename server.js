6a3618d38bb1f7a04a6f152e1986ca55585d4c41
PachaiyappanV <pachaiyappan394@gmail.com>
2025-03-02 19:34:52 +0530

    feat: refactor video upload process to streamline S3 upload and transcription handling

--- server.js
+++ server.js
@@ -51,108 +51,107 @@ app.post("/upload", upload.single("video"), async (req, res) => {
 
   const audioPath = await extractAudioAndCompress(fileName);
   console.log("🟢 Audio Path:", audioPath);
-  try {
-    // upload video to s3
-    fs.readFile("temp_video/" + fileName, async (err, file) => {
-      const processing = await axios.post(
-        `${process.env.NEXT_API_HOST}/recording/${userId}/processing`,
-        { fileName }
+
+  // upload video to s3
+  fs.readFile("temp_video/" + fileName, async (err, file) => {
+    const processing = await axios.post(
+      `${process.env.NEXT_API_HOST}/recording/${userId}/processing`,
+      { fileName }
+    );
+
+    if (processing.data.status !== 200)
+      return console.log(
+        "🔴 Error: Something went wrong with creating the processing file"
       );
 
-      if (processing.data.status !== 200)
-        return console.log(
-          "🔴 Error: Something went wrong with creating the processing file"
-        );
+    const Key = fileName;
+    const Bucket = process.env.AWS_BUCKET_NAME;
+    const ContentType = "video/webm";
 
-      const Key = fileName;
-      const Bucket = process.env.AWS_BUCKET_NAME;
-      const ContentType = "video/webm";
+    const command = new PutObjectCommand({
+      Key,
+      Bucket,
+      ContentType,
+      Body: file,
+    });
 
-      const command = new PutObjectCommand({
-        Key,
-        Bucket,
-        ContentType,
-        Body: file,
-      });
+    const fileStatus = await s3.send(command);
 
-      const fileStatus = await s3.send(command);
+    if (fileStatus["$metadata"].httpStatusCode === 200) {
+      console.log("🟢 Video Uploaded To AWS");
 
-      if (fileStatus["$metadata"].httpStatusCode === 200) {
-        console.log("🟢 Video Uploaded To AWS");
+      if (processing.data.plan === "PRO") {
+        fs.stat(audioPath, async (err, stat) => {
+          if (!err) {
+            // Whisper 25MB limit
+            if (stat.size < 25000000) {
+              const transcription = await openai.audio.transcriptions.create({
+                file: fs.createReadStream(audioPath),
+                model: "whisper-1",
+                response_format: "text",
+              });
 
-        if (processing.data.plan === "PRO") {
-          fs.stat(audioPath, async (err, stat) => {
-            if (!err) {
-              // Whisper 25MB limit
-              if (stat.size < 25000000) {
-                const transcription = await openai.audio.transcriptions.create({
-                  file: fs.createReadStream(audioPath),
-                  model: "whisper-1",
-                  response_format: "text",
-                });
-
-                if (transcription) {
-                  const completion = await openai.chat.completions.create({
-                    model: "gpt-3.5-turbo",
-                    response_format: { type: "json_object" },
-                    temperature: 0.7,
-                    messages: [
-                      {
-                        role: "system",
-                        content: `You are going to generate a title and a nice description using the speech-to-text transcription provided: transcription(${transcription}) and then return it in JSON format as {"title": <the title you gave>, "summary": <the summary you created>}`,
-                      },
-                    ],
-                  });
-                  console.log("Transcription", transcription);
-                  console.log("Content", completion.choices[0].message.content);
-                  const titleAndSummaryGenerated = await axios.post(
-                    `${process.env.NEXT_API_HOST}/recording/${userId}/transcribe`,
+              if (transcription) {
+                const completion = await openai.chat.completions.create({
+                  model: "gpt-3.5-turbo",
+                  response_format: { type: "json_object" },
+                  temperature: 0.7,
+                  messages: [
                     {
-                      fileName,
-                      content: completion.choices[0].message.content,
-                      transcription: transcription,
-                    }
-                  );
-
-                  if (titleAndSummaryGenerated.data.status !== 200) {
-                    console.log(
-                      "🔴 Error: Something went wrong with creating the title and summary file"
-                    );
+                      role: "system",
+                      content: `You are going to generate a title and a nice description using the speech-to-text transcription provided: transcription(${transcription}) and then return it in JSON format as {"title": <the title you gave>, "summary": <the summary you created>}`,
+                    },
+                  ],
+                });
+                console.log("Transcription", transcription);
+                console.log("Content", completion.choices[0].message.content);
+                const titleAndSummaryGenerated = await axios.post(
+                  `${process.env.NEXT_API_HOST}/recording/${userId}/transcribe`,
+                  {
+                    fileName,
+                    content: completion.choices[0].message.content,
+                    transcription: transcription,
                   }
+                );
+
+                if (titleAndSummaryGenerated.data.status !== 200) {
+                  console.log(
+                    "🔴 Error: Something went wrong with creating the title and summary file"
+                  );
                 }
               }
             }
-          });
-        }
-
-        const stopProcessing = await axios.post(
-          `${process.env.NEXT_API_HOST}/recording/${userId}/complete`,
-          { fileName }
-        );
-
-        if (stopProcessing.data.status !== 200)
-          console.log(
-            "🔴 Error: Something went wrong when stopping the process and trying to complete the processing stage."
-          );
-      } else {
-        console.log("🔴 Error: Something went wrong with uploading the video");
-        return res.status(500).json({ error: "Something went wrong" });
+          }
+        });
       }
-    });
 
-    res.status(201).json({ message: "File uploaded successfully" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Something went wrong" });
-  } finally {
-    fs.unlink(`temp_video/${fileName}`, (err) => {
-      if (!err) console.log("🟢 Video Deleted");
-    });
+      const stopProcessing = await axios.post(
+        `${process.env.NEXT_API_HOST}/recording/${userId}/complete`,
+        { fileName }
+      );
 
-    fs.unlink(audioPath, (err) => {
-      if (!err) console.log("🟢 Audio Deleted");
-    });
-  }
+      if (stopProcessing.data.status !== 200) {
+        console.log(
+          "🔴 Error: Something went wrong when stopping the process and trying to complete the processing stage."
+        );
+      }
+      if (stopProcessing.data.status === 200) {
+        console.log("🟢 Processing Completed");
+        fs.unlink(`temp_video/${fileName}`, (err) => {
+          if (!err) console.log("🟢 Video Deleted");
+        });
+
+        fs.unlink(audioPath, (err) => {
+          if (!err) console.log("🟢 Audio Deleted");
+        });
+      }
+    } else {
+      console.log("🔴 Error: Something went wrong with uploading the video");
+      return res.status(500).json({ error: "Something went wrong" });
+    }
+  });
+
+  res.status(201).json({ message: "File uploaded successfully" });
 });
 app.get("/", (req, res) => {
   res.send("Hello World!");
